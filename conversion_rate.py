be2c1333206b5ebf24d89482635e2903d2c3e99c
Danijel <misulicd@gmail.com>
2019-08-25 19:03:36 +0200

    Added decsriptive statistics

--- conversion_rate.py
+++ conversion_rate.py
@@ -7,7 +7,8 @@ Created on Sun Aug 25 18:13:46 2019
 
 import pandas as pd
 
-pace_data = pd.read_csv('C:/Users/Danijel/Desktop/PLINI_PACE_0519-1.csv', delimiter=',', index_col=None)
+#import data and define column names since they are missing
+pace_data = pd.read_csv('C:/Users/Danijel/Desktop/PLINI_PACE_0519-1.csv', delimiter=',', names=["date", "company", "item_product_id", "customer_id", "item_name", "licence_type"], index_col=None)
 
 #DATA PREPROCESSING AND CLEANING
 
@@ -19,14 +20,22 @@ pace_data.columns
 print (pace_data.dtypes)
 
 #check for missing values
-print (pace_data[pace_data["CODEREDEMPTION"].isnull() == True].shape)
-#print (pace_data.duplicated())
+pace_data[pace_data["date"].isnull() == True].shape
+pace_data[pace_data["company"].isnull() == True].shape
+pace_data[pace_data["item_product_id"].isnull() == True].shape
+pace_data[pace_data["customer_id"].isnull() == True].shape
+pace_data[pace_data["item_name"].isnull() == True].shape
+pace_data[pace_data["licence_type"].isnull() == True].shape
 
-#pace_data["account_status"].unique()
+#check for duplicated rows
+pace_data[pace_data.duplicated() == True].shape
 
+#check for unique values for date and user ids
+print (len(pace_data["date"].unique()))
+print (len(pace_data["customer_id"].unique()))
 
-pace_data['origin'] = "plini"
+#add origin column before merging data
+pace_data['origin'] = "pace"
 
-#print (pace_data)
-
-
+#calculate number of full licences
+print (pace_data[pace_data['licence_type'] == "FULL"].shape)
